78838d5a4cf087af472a0da7c9d67937b3b036fb
Alexey Rusakov <Kitsune-Ral@users.sf.net>
2022-09-11 13:57:30 +0200

    CREATE_CONNECTION -> createTestConnection

--- autotests/testkeyverification.cpp
+++ autotests/testkeyverification.cpp
@@ -7,6 +7,8 @@
 #include "testutils.h"
 #include <qt_connection_util.h>
 
+#include <QtTest/QSignalSpy>
+
 class TestKeyVerificationSession : public QObject
 {
     Q_OBJECT
@@ -17,41 +19,54 @@ private Q_SLOTS:
         CREATE_CONNECTION(a, "alice1", "secret", "AliceDesktop")
         CREATE_CONNECTION(b, "alice1", "secret", "AlicePhone")
 
-        QPointer<KeyVerificationSession> aSession{};
-        connect(a.get(), &Connection::newKeyVerificationSession, this, [&](KeyVerificationSession* session) {
-            aSession = session;
-            QVERIFY(session->remoteDeviceId() == b->deviceId());
-            QVERIFY(session->state() == KeyVerificationSession::WAITINGFORREADY);
-            connectSingleShot(session, &KeyVerificationSession::stateChanged, this, [=](){
-                QVERIFY(session->state() == KeyVerificationSession::ACCEPTED || session->state() == KeyVerificationSession::READY);
-                connectSingleShot(session, &KeyVerificationSession::stateChanged, this, [=](){
-                    QVERIFY(session->state() == KeyVerificationSession::WAITINGFORVERIFICATION);
-                    session->sasVerified();
+        using KVS = KeyVerificationSession;
+	    QPointer<KVS> aSession{};
+        connect(a.get(), &Connection::newKeyVerificationSession, this,
+                [&](KVS* session) {
+                    aSession = session;
+                    QVERIFY(session->remoteDeviceId() == b->deviceId());
+                    QVERIFY(session->state() == KVS::WAITINGFORREADY);
+                    connectSingleShot(
+                        session, &KVS::stateChanged, this, [this, session] {
+                            QVERIFY(session->state() == KVS::ACCEPTED);
+                            connectSingleShot(
+                                session, &KVS::stateChanged, this, [session] {
+                                    QVERIFY(session->state()
+                                            == KVS::WAITINGFORVERIFICATION);
+                                    session->sasVerified();
+                                });
+                        });
                 });
-            });
-        });
         a->startKeyVerificationSession(b->deviceId());
-        connect(b.get(), &Connection::newKeyVerificationSession, this, [=](KeyVerificationSession* session) {
-            QVERIFY(session->remoteDeviceId() == a->deviceId());
-            QVERIFY(session->state() == KeyVerificationSession::INCOMING);
-            session->setReady();
-            // KeyVerificationSession::READY is skipped because we have only one method
-            QVERIFY(session->state() == KeyVerificationSession::WAITINGFORACCEPT);
-            connectSingleShot(session, &KeyVerificationSession::stateChanged, this, [=](){
-                QVERIFY(session->state() == KeyVerificationSession::WAITINGFORKEY || session->state() == KeyVerificationSession::ACCEPTED);
-                connectSingleShot(session, &KeyVerificationSession::stateChanged, this, [=]() {
-                    QVERIFY(session->state() == KeyVerificationSession::WAITINGFORVERIFICATION);
-                    QVERIFY(aSession);
-                    QVERIFY(aSession->sasEmojis() == session->sasEmojis());
-                    session->sasVerified();
-                    QVERIFY(session->state() == KeyVerificationSession::WAITINGFORMAC);
+        connect(b.get(), &Connection::newKeyVerificationSession, this,
+                [this, a](KVS* session) {
+                    QVERIFY(session->remoteDeviceId() == a->deviceId());
+                    QVERIFY(session->state() == KVS::INCOMING);
+                    session->setReady();
+                    // KeyVerificationSession::READY is skipped because we have
+                    // only one method
+                    QVERIFY(session->state() == KVS::WAITINGFORACCEPT);
+                    connectSingleShot(
+                        session, &KVS::stateChanged, this, [this, session] {
+                            QVERIFY(session->state() == KVS::WAITINGFORKEY
+                                    || session->state() == KVS::ACCEPTED);
+                            connectSingleShot(
+                                session, &KVS::stateChanged, this,
+                                [this, session] {
+                                    QVERIFY(session->state()
+                                            == KVS::WAITINGFORVERIFICATION);
+                                    QVERIFY(aSession);
+                                    QVERIFY(aSession->sasEmojis()
+                                            == session->sasEmojis());
+                                    session->sasVerified();
+                                    QVERIFY(session->state()
+                                            == KVS::WAITINGFORMAC);
+                                });
+                        });
                 });
-            });
-
-        });
         b->syncLoop();
         a->syncLoop();
-        QSignalSpy spy(aSession, &KeyVerificationSession::finished);
+        QSignalSpy spy(aSession, &KVS::finished);
         spy.wait(10000);
     }
 };
